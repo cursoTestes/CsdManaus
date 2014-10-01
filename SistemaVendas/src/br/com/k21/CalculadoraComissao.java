702376133ab797e7599c5246088d9651f4ee6c3e
cfc <cfc@ufrj.br>
2014-10-01 15:58:11 -0300

    msg  commit_csd_1

---
+++ SistemaVendas/src/br/com/k21/CalculadoraComissao.java
@@ -0,0 +1,9 @@
+package br.com.k21;
+
+public class CalculadoraComissao {
+
+	public static Double comissao(Double venda) {
+		return (venda * 0.05);
+	}
+
+}
---
+++ SistemaVendas/testes/br/com/k21/TestCalculadoraComissao.java
@@ -0,0 +1,53 @@
+package br.com.k21;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCalculadoraComissao {
+	
+	@Test
+	public void calcula_Venda_de_100_Retorna_5() {
+		Double venda = 100.0;
+		Double esperado = 5.0;
+		
+		Double comissao = CalculadoraComissao.comissao(venda);  
+		
+		assertEquals(esperado, comissao);
+	
+	}
+	
+	@Test
+	public void calcula_Venda_de_10000_Retorna_500() {
+		Double venda = 10000.0;
+		Double esperado = 500.0;
+		
+		Double comissao = CalculadoraComissao.comissao(venda);  
+		
+		assertEquals(esperado, comissao);
+	
+	}
+	
+	@Test
+	public void calcula_Venda_de_1_Retorna_5_Centavos() {
+		Double venda = 1D;
+		Double esperado = 0.05;
+		
+		Double comissao = CalculadoraComissao.comissao(venda);  
+	
+		assertEquals(esperado, comissao);
+	
+	}
+	
+	@Test
+	public void calcula_Venda_de_50_Centavos_Retorna_0025_Centavos() {
+		Double venda = 0.5;
+		Double esperado = 0.025;
+		
+		Double comissao = CalculadoraComissao.comissao(venda);  
+	
+		assertEquals(esperado, comissao);
+	
+	}
+	
+}
